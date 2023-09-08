77d736ded8ac38a8800521927c77b21d1662e387
TarasMysiura <tarasmysiura@gmail.com>
2023-09-08 23:46:04 +0300

    add

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ import app from "./app.js";
 
 
 
-// const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose.connect(process.env.DB_HOST)
+mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
-    console.log("Server running. Use our API on port: 3000")
+    console.log(`Server running. Use our API on port: ${PORT}`)
     })
   })
   .catch(error => {
